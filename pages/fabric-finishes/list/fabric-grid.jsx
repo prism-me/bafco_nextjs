30e2141fdf6f1bf6d5c8a95ebff6b3e3a0e3c000
faiza-zakir <faiza.prism@gmail.com>
2022-10-25 15:59:27 +0500

    fabric grid code cleaning

--- pages/fabric-finishes/list/fabric-grid.jsx
+++ pages/fabric-finishes/list/fabric-grid.jsx
@@ -1,15 +1,7 @@
-import React, { useState } from "react";
-// import { useRouter } from "next/router";
-// import { connect } from "react-redux";
+import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
-// import { actions as wishlistAction } from "~/store/wishlist";
-// import { actions as cartAction } from "~/store/cart";
-// import { actions as compareAction } from "~/store/compare";
-// import { actions as demoAction } from "~/store/demo";
-
 function FabricGrid(props) {
-  // const router = useRouter();
   const { product, setIsOpen, setProductId } = props;
 
   return (
@@ -26,19 +18,7 @@ function FabricGrid(props) {
             setProductId(product?.id);
           }}
         />
-        {/* {product?.featured_img ? (
-          <LazyLoadImage
-            alt="product"
-            src={product?.featured_img}
-            threshold={500}
-            effect="black and white"
-            wrapperClassName="product-image-hover"
-          />
-        ) : (
-          ""
-        )} */}
       </figure>
-
       <div className="product-body">
         <h3 className="product-title">{product?.name}</h3>
       </div>
@@ -46,18 +26,4 @@ function FabricGrid(props) {
   );
 }
 
-// const mapStateToProps = (state) => {
-//   return {
-//     wishlist: state.wishlist.data,
-//     comparelist: state.comparelist.data,
-//   };
-// };
-
-export default // connect(mapStateToProps, {
-//   ...wishlistAction,
-//   ...cartAction,
-//   ...compareAction,
-//   ...demoAction,
-// })(
-FabricGrid;
-// );
+export default FabricGrid;
