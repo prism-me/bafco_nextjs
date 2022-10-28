e0a62ce024b2df9e6407f85b4d878e576fa0bec2
Payal Bhisikar <devteam2@prism-me.com>
2022-10-28 16:36:03 +0400

    added google tag manager code.

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -16,6 +16,21 @@ export default class MyDocument extends Document {
             type="text/css"
             href="css/fonts-molla.min.css"
           />
+
+          {/* <!-- Google Tag Manager --> */}
+          <script
+            data-partytown-config
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5P3QZ4C');
+            `,
+            }}
+          />
+
           {/* <link
             rel="stylesheet"
             type="text/css"
