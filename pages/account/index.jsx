40af4fe0bcc6ac12320f129b6f824d58ebaba4e3
faiza-zakir <faiza.prism@gmail.com>
2022-11-22 18:14:07 +0500

    show order cancellation msg

--- pages/account/index.jsx
+++ pages/account/index.jsx
@@ -613,7 +613,6 @@ function MyAccount(props) {
     setCancelMessage(updatedmsg);
   };
 
-  const [disableButton, setDisableButton] = useState(false);
   const handleOrderCancel = (id, status) => {
     let UserId = localStorage.getItem("UserData");
 
@@ -643,7 +642,6 @@ function MyAccount(props) {
           })
             .then((response) => {
               setOrderList(response?.data);
-              setDisableButton(true);
             })
             .catch((err) => console.log(err));
         })
@@ -811,56 +809,57 @@ function MyAccount(props) {
                                       <td className="td_product">
                                         <span>{order?.total}</span>
                                       </td>
-                                      <td className="td_product">
-                                        <span
-                                          onClick={() => {
-                                            setSingleOrderID(order?.id);
-                                            openOrderDetailModal();
-                                          }}
-                                        >
-                                          <svg
-                                            stroke="currentColor"
-                                            fill="currentColor"
-                                            stroke-width="0"
-                                            viewBox="0 0 576 512"
-                                            className="action-icon-details"
-                                            height="20"
-                                            width="20"
-                                            xmlns="http://www.w3.org/2000/svg"
+                                      {order?.status ===
+                                      "ORDERCANCELLLATIONREQUEST" ? (
+                                        <td className="td_product">
+                                          <span className="text-danger">
+                                            Your Order Cancellation Request is
+                                            under Process
+                                          </span>
+                                        </td>
+                                      ) : (
+                                        <td className="td_product">
+                                          <span
+                                            onClick={() => {
+                                              setSingleOrderID(order?.id);
+                                              openOrderDetailModal();
+                                            }}
                                           >
-                                            <path d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z"></path>
-                                          </svg>
-                                        </span>
-                                        <a
-                                          href={`/account/order/${order?.order_number}`}
-                                          className="track_order"
-                                        >
-                                          Track shipment
-                                        </a>
-                                        <button
-                                          className="track_order"
-                                          style={{
-                                            background: "#ee3124",
-                                            cursor: `${
-                                              disableButton === true &&
-                                              "not-allowed"
-                                            }`,
-                                          }}
-                                          onClick={() => {
-                                            setShowReasonModal(true);
-                                            setOrderCancelNumber(
-                                              order?.order_number
-                                            );
-                                          }}
-                                          disabled={
-                                            disableButton === true
-                                              ? true
-                                              : false
-                                          }
-                                        >
-                                          Cancel
-                                        </button>
-                                      </td>
+                                            <svg
+                                              stroke="currentColor"
+                                              fill="currentColor"
+                                              stroke-width="0"
+                                              viewBox="0 0 576 512"
+                                              className="action-icon-details"
+                                              height="20"
+                                              width="20"
+                                              xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                              <path d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z"></path>
+                                            </svg>
+                                          </span>
+                                          <a
+                                            href={`/account/order/${order?.order_number}`}
+                                            className="track_order"
+                                          >
+                                            Track shipment
+                                          </a>
+                                          <button
+                                            className="track_order"
+                                            style={{
+                                              background: "#ee3124",
+                                            }}
+                                            onClick={() => {
+                                              setShowReasonModal(true);
+                                              setOrderCancelNumber(
+                                                order?.order_number
+                                              );
+                                            }}
+                                          >
+                                            Cancel
+                                          </button>
+                                        </td>
+                                      )}
                                     </tr>
                                   ))}
                                 </tbody>
