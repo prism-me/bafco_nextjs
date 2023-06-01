0d67593a0db362ba6a88c84e826ce73bccc168a8
yar muhammad <devteam2@prism-me.com>
2023-06-01 15:18:21 +0500

    Update API.js

--- http/API.js
+++ http/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { store } from "~/store";
 
-const apiURL = "https://endpoint.bafco.com/v1/api/auth/login/";
+const apiURL = "https://endpoint.bafco.com/v1/api/";
 export const API = axios.create({
   baseURL: apiURL,
   timeout: 90000,
